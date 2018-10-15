fde32f7a3fc6c92f76f3fd2badc8da160fe7464d
DarthChurros <tnarayan00@stuy.edu>
2018-10-15 17:28:12 -0400

    Finished RationalNumber class

---
+++ RationalNumber.java
@@ -0,0 +1,14 @@
+public class RationalNumber extends RealNumber{
+  private int num;
+  private int denom;
+
+  public RationalNumber (int a, int b) {
+    super(1.0 * a / b);
+    num = a;
+    denom = b;
+  }
+
+  public boolean greaterThan(RationalNumber other) {
+    return true;
+  }
+}
